130f95f317fbca6295e201c0fb4aad3a992da8a5
Sheena Samuel <45675052+SheenaSamuel@users.noreply.github.com>
2020-02-20 07:48:59 -0500

    Update BinomialTheorem.java

--- BinomialTheorem.java
+++ BinomialTheorem.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class MyClass {
     public static void main(String args[]) {
-     int x = 3;
-     int y = 5;
+     int x = 1;
+     int y = 1;
      
      BinomialTheorem bt = new BinomialTheorem();
      
@@ -41,12 +41,16 @@ class BinomialTheorem{
             while (a<temp.length){
                 temp[0]= (int)Math.pow(x,c);
                 temp[temp.length-1] = (int)Math.pow(y,c);
-                
-                
-                
                 a++;
             }
             tngl[c] = temp;
+        }
+        for(int c = 2; c<tngl.length; c++){
+            for (int w = 1; w<tngl[c].length; w++){
+                if (tngl[c][w]==0){
+                    tngl[c][w]=tngl[c-1][w]+tngl[c-1][w-1];
+                }
+            }
         }
         
      return tngl;   
